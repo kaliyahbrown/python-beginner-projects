8c26e6c4f5ecbf0cd0dcefe1fa1291b85433451a
kaliyahbrown <kcbrown118@gmail.com>
2026-08-10 15:59:10 -0400

    beginner calculator project

---
+++ calculator.py
@@ -0,0 +1,9 @@
+print("Simple Calculator")
+
+num1 = float(input("Enter your first number: "))
+num2 = float(input("Enter your second number: "))
+
+print("Add:", num1 + num2)
+print("Subtract:", num1 - num2)
+print("Multiply:", num1 * num2)
+print("Divide:", num1 / num2)
