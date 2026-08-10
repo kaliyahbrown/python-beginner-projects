5a9d65590c3cf2ae7f6c495081f5d2139e46ef22
kaliyahbrown <kcbrown118@gmail.com>
2026-08-10 16:24:49 -0400

    Update number_guessing_game.py

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -9,6 +9,7 @@ while guess != secret_number:
     guess = int(input("Guess a number between 1 and 10: "))
 
     if guess < secret_number:
+        print("Too low!")
     elif guess > secret_number:
         print("Too high!")
     else:
