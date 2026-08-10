f441c04ffb2f438414999ab1efa6c02e7cf6b92a
kaliyahbrown <kcbrown118@gmail.com>
2026-08-10 16:24:46 -0400

    Add number guessing game

---
+++ number_guessing_game.py
@@ -0,0 +1,15 @@
+import random
+
+print("Number Guessing Game")
+
+secret_number = random.randint(1, 10)
+guess = 0
+
+while guess != secret_number:
+    guess = int(input("Guess a number between 1 and 10: "))
+
+    if guess < secret_number:
+    elif guess > secret_number:
+        print("Too high!")
+    else:
+        print("Correct! You guessed the number!")
